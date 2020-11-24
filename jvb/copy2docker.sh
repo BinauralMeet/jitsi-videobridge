6c94f42b568cf3dcb7aa07d9218a833bd07c0abc
Shoichi Hasegawa <hasevr@gmail.com>
2020-11-24 23:19:17 +0900

    copy to octo test

--- jvb/copy2docker.sh
+++ jvb/copy2docker.sh
@@ -6,5 +6,8 @@ rm -rf myjvb
 unzip ../jitsi-videobridge/jvb/target/jitsi-videobridge-2.1-SNAPSHOT-archive.zip
 mv jitsi-videobridge-2.1-SNAPSHOT myjvb
 echo このフォルダがdockerで動くjvb。debian/jitsi-videobridgeが上書きする。> myjvb/readme.txt
-cp ../jitsi-videobridge/jvb/resources/jvb.sh ./jvb/
+rm -rf /c/users/hase/.jitsi-meet-cfg/myjvb
+cp -a myjvb /c/users/hase/.jitsi-meet-cfg/
+rm -rf /c/users/hase/.jitsi-meet-cfg2/myjvb
+cp -a myjvb /c/users/hase/.jitsi-meet-cfg2/
 popd
