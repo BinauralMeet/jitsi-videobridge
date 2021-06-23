483a7915cb43a9d0120da373aacfc4b6642b0ed7
Shoichi Hasegawa <hasevr@gmail.com>
2021-06-23 23:20:08 +0900

    script to install jitsi's debian package for this jvb.

---
+++ jvb/installDebianPackage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get install jicofo=1.0-740-1
+apt-get install jitsi-meet-web=1.0.4900-1
+apt-get install jitsi-meet-web-config=1.0.4900-1
+apt-get install jitsi-meet-prosody=1.0.4900-1
+apt-get install jitsi-meet-turnserver=1.0.4900-1
+apt-get install jitsi-videobridge2=2.1-478-gc6da57bd-1
+apt-get install jitsi-meet=2.0.5765-1
